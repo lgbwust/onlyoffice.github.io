a0351da57342dc2ab5840791ccbebe247a00c316
iceliu <1461976922@qq.com>
2024-01-09 10:08:06 +0800

    Update main.js

--- sdkjs-plugins/content/ICE/main.js
+++ sdkjs-plugins/content/ICE/main.js
@@ -1,31 +1,38 @@
-// 假设你要查找的书签的名字是"myBookmark"
-var bookmarkName = "myBookmark";
+(function(window, undefined) {
+  window.Asc.plugin.init = function(initData) {
+    var me = this
+    $('#addText').click(function() {
+      // 官方提供的回调函数，所有操作文档的 API 都可以在这里面使用
+      me.callCommand(function() {
+        try {
+          // 获取文档对象
+          var oDocument = Api.GetDocument()
+          // 生成一个新的段落对象
+          var oParagraph = Api.CreateParagraph()
+          // 往段落里面添加一个字符串文本
+          oParagraph.AddText('Hello world')
+          // 最后往文档里面添加一个段落对象
+          oDocument.Push(oParagraph)
+        } catch (error) {
+          console.error(error)
+        }
+      }, false, true, function () {
+        console.log('ok')
+      })
+    })
 
-// 假设你的REST接口的URL是"https://my-api.com/image"
-var imageUrl = "https://my-api.com/image";
-
-window.Asc.plugin.init = function () {
-    document.getElementById("insertImage").addEventListener("click", function () {
-        fetch(imageUrl)
-            .then(response => response.blob())
-            .then(blob => {
-                var reader = new FileReader();
-                reader.onload = function () {
-                    var base64Image = reader.result;
-                    window.Asc.plugin.executeMethod("AddImageUrl", [bookmarkName, base64Image]);
-                };
-                reader.readAsDataURL(blob);
-            })
-            .catch(error => console.error(error));
-    });
-};
-
-window.Asc.plugin.onMethodReturn = function (returnValue) {
-    if (returnValue.methodName == "AddImageUrl") {
-        this.executeCommand("close", "");
+    // 在插件 iframe 之外释放鼠标按钮时调用的函数
+    window.Asc.plugin.onExternalMouseUp = function() {
+      var event = document.createEvent('MouseEvents')
+      event.initMouseEvent('mouseup', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null)
+      document.dispatchEvent(event)
     }
-};
 
-window.Asc.plugin.button = function (id) {
-    this.executeCommand("close", "");
-};
+    window.Asc.plugin.button = function(id) {
+      // 被中断或关闭窗口
+      if (id === -1) {
+        this.executeCommand('close', '')
+      }
+	  }
+  }
+})(window, undefined)
