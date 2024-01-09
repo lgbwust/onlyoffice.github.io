9eedec1bebc71a3f141448f0d7e4be172d2d21d9
iceliu <1461976922@qq.com>
2024-01-09 12:33:15 +0800

    Update main.js

--- sdkjs-plugins/content/ICE/main.js
+++ sdkjs-plugins/content/ICE/main.js
@@ -5,11 +5,34 @@
       // 官方提供的回调函数==>所有操作文档的 API 都可以在这里面使用
       me.callCommand(function() {
         try {
-		var oDocument = Api.GetDocument();
-                var oParagraph = Api.CreateParagraph();
-                var oImage = Api.CreateImage("https://gzjc-file.ceczy.com/get-file/group1/M00/00/23/rBAAdWSJJRuAKXxFAAAMzQLbvpU491.png", 60 * 36000, 60 * 36000);
-                oParagraph.AddDrawing(oImage);
-                oDocument.InsertContent([oParagraph]);
+		// var oDocument = Api.GetDocument();
+  //               var oParagraph = Api.CreateParagraph();
+  //               var oImage = Api.CreateImage("https://gzjc-file.ceczy.com/get-file/group1/M00/00/23/rBAAdWSJJRuAKXxFAAAMzQLbvpU491.png", 60 * 36000, 60 * 36000);
+  //               oParagraph.AddDrawing(oImage);
+  //               oDocument.InsertContent([oParagraph]);
+
+		 var oDocument = Api.GetDocument();
+               var oBookmarks = oDocument.GetBookmarks(); // 获取所有书签
+
+    // 查找名为 "pic" 的书签
+    var oBookmark = oBookmarks.find(function(bookmark) {
+        return bookmark.GetName() === "pic";
+    });
+
+    if (oBookmark) {
+        var oRange = oBookmark.GetRange(); // 获取书签的范围
+        var oParagraph = oRange.GetParagraph(); // 获取书签所在的段落
+
+        // 创建并添加图片
+        var oImage = Api.CreateImage("https://api.onlyoffice.com/content/img/docbuilder/examples/api-cell.png", 60 * 36000, 60 * 36000);
+        oParagraph.AddDrawing(oImage);
+
+        // 将图片插入到书签位置
+        oRange.Replace(oParagraph);
+    } else {
+        console.log("Bookmark 'pic' not found.");
+    }
+		
         } catch (error) {
           console.error(error)
         }
