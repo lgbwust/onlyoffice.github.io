7993807c385fe8aab1d3637a488d84d54cb2d411
iceliu <1461976922@qq.com>
2024-01-09 10:29:54 +0800

    Update main.js

--- sdkjs-plugins/content/ICE/main.js
+++ sdkjs-plugins/content/ICE/main.js
@@ -5,14 +5,34 @@
       // 官方提供的回调函数，所有操作文档的 API 都可以在这里面使用
       me.callCommand(function() {
         try {
-          // 获取文档对象
-          var oDocument = Api.GetDocument()
-          // 生成一个新的段落对象
-          var oParagraph = Api.CreateParagraph()
-          // 往段落里面添加一个字符串文本
-          oParagraph.AddText('Hello world')
-          // 最后往文档里面添加一个段落对象
-          oDocument.Push(oParagraph)
+          // // 获取文档对象
+          // var oDocument = Api.GetDocument()
+          // // 生成一个新的段落对象
+          // var oParagraph = Api.CreateParagraph()
+          // // 往段落里面添加一个字符串文本
+          // oParagraph.AddText('Hello world')
+          // // 最后往文档里面添加一个段落对象
+          // oDocument.Push(oParagraph)
+	  // 获取文档对象
+            var oDocument = Api.GetDocument()
+            // 从网络获取图片
+            var imageUrl = ' https://gzjc-file.ceczy.com/get-file/group1/M00/00/23/rBAAdWSJJRuAKXxFAAAMzQLbvpU491.png' // 请替换为你的图片 URL
+            var xhr = new XMLHttpRequest()
+            xhr.open('GET', imageUrl, true)
+            xhr.responseType = 'blob'
+            xhr.onload = function() {
+              if (this.status === 200) {
+                var reader = new FileReader()
+                reader.onloadend = function() {
+                  // 创建一个新的图片对象
+                  var oImage = Api.CreateImage(reader.result)
+                  // 往文档里面添加一个图片对象
+                  oDocument.Push(oImage)
+                }
+                reader.readAsDataURL(this.response)
+              }
+            }
+            xhr.send()
         } catch (error) {
           console.error(error)
         }
