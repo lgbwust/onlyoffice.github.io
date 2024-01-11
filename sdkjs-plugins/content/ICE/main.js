b2c3c0f8cee3ce11c4ce077ed9ac443854e268de
iceliu <1461976922@qq.com>
2024-01-11 16:20:20 +0800

    Update main.js

--- sdkjs-plugins/content/ICE/main.js
+++ sdkjs-plugins/content/ICE/main.js
@@ -70,6 +70,16 @@ console.log(base64);
     }
 
     window.Asc.plugin.button = function(id) {
+
+	    var _info = window.Asc.plugin.info;
+    var _method = (_info.objectId === undefined) ? "asc_addOleObject" : "asc_editOleObject";
+    _info.width = _info.width ? _info.width : 70;
+    _info.height = _info.height ? _info.height : 70;
+    _info.widthPix = (_info.mmToPx * _info.width) >> 0;
+    _info.heightPix = (_info.mmToPx * _info.height) >> 0;
+    _info.imgSrc = window.g_board.getResult(_info.widthPix, _info.heightPix).image;
+    _info.data = window.g_board.getData();
+    var _code = "Api." + _method + "(" + JSON.stringify(_info) + ");";
       if (id === -1) {
         this.executeCommand('close', '')
       }
