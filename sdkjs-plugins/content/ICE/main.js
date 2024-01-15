7268b624e27365c4fb1ad46a3bf5ba14f5010984
iceliu <1461976922@qq.com>
2024-01-15 15:44:50 +0800

    Update main.js

--- sdkjs-plugins/content/ICE/main.js
+++ sdkjs-plugins/content/ICE/main.js
@@ -1,25 +1,27 @@
+var imgData = '';
 (function (window, undefined) {
     window.Asc.plugin.init = function (initData) {
         debugger;
         var callback = decodeURIComponent(window.Asc.plugin.info.documentCallbackUrl);
         var url = new URL(callback);
         var params = new URLSearchParams(url.search);
-        initData = params.get("imgData");
-        console.log(initData);
-        this.initData=initData;
+        imgData = params.get("imgData");
+        console.log(imgData);
+        this.imgData=imgData;
         var me = this;
         $('#addText').click(function () {
             // 官方提供的回调函数==>所有操作文档的 API 都可以在这里面使用
             console.log("=======>");
-            console.log(initData);
+            console.log(imgData);
             me.callCommand(function () {
                 try {
+                    debugger
                     console.log("=======>");
-                    console.log(me.initData);
+                    console.log(imgData);
                     var oDocument = Api.GetDocument();
                     var oRange = oDocument.GetBookmarkRange("pic"); // 获取名为 "pic" 的书签的范围
                     var oParagraph = oRange.GetAllParagraphs(1);
-                    var oImage = Api.CreateImage(me.initData, 30 * 36000, 30 * 36000);
+                    var oImage = Api.CreateImage(imgData, 30 * 36000, 30 * 36000);
                     oParagraph[0].AddDrawing(oImage);
 
                 } catch (error) {
